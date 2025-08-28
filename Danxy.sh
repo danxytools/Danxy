23e4df773621f1ac177e32480230c04ccc79eb48
Your Name <you@example.com>
2025-08-28 14:46:20 +0700

    Update user via DanxyAssistent.sh

--- Danxy.sh
+++ Danxy.sh
@@ -11,6 +11,7 @@ BG_GREEN='\033[1;42m' BG_RED='\033[1;41m' BG_YELLOW='\033[1;43m'
 
 # DATABASE WHOAMIN
 USER_MAP=(
+    "iqbal|u0_a441"
     "Mr.FlowX|u0_a257"
     "Razor|u0_a485"
     "Her1980|u0_a348"
