61cadc8913af4e8a8f368255166938f2ead69f4c
danxytools <danxytoolsv8.3@gmail.com>
2025-09-06 00:06:21 +0700

    Update DanxyToolsV8.3

--- Danxy.sh
+++ Danxy.sh
@@ -11,6 +11,7 @@ BG_GREEN='\033[1;42m' BG_RED='\033[1;41m' BG_YELLOW='\033[1;43m'
 
 # DATABASE WHOAMIN
 USER_MAP=(
+    "rahmanz|u0_a2817"
     "Danxy|u0_a369"
     "flay|u0_a620"
     "Yama|u0_a252"
